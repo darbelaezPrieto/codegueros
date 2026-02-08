c2c93933c9cfa4ea995c67f1a69505d4eeceb54f
alejo <alejandrosanta08@gmail.com>
2026-02-07 21:51:51 -0500

    se sube ejercicio 4

--- proyecto/src/pruebas.java
+++ proyecto/src/pruebas.java
@@ -1,8 +1,20 @@
 
+class Scratch {
+    public static int sumarnumeropar(int numero){
+        int suma = 0;
+        int i;
+        for (i = 1; i <= numero; i++) {
+            if (i%2==0) {
+                suma+=i;
+            }
+        }
+        return suma;
 
-public class pruebas {
-
-    public static void main(String [] args){
 
     }
+
+    public static void main(String arg[]){
+        System.out.println(sumarnumeropar( 8));
+    }
+
 }
